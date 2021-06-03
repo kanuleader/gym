529effb648005f1a84f5ee2fdf24fe1375a7f01d
Kanuleader <kanuleader@gmail.com>
2021-06-04 00:23:14 +0900

    Updated

--- log.cpp
+++ log.cpp
@@ -1,7 +1,19 @@
+#include <stdio.h>
+
+#define DEBUG
+
 #ifdef DEBUG
-#define PINT()    printf("pin: [%s, %d]: %s: %d￦n", __FUNCTION__, __LINE__, #a, (a))
-
+#define PINT(a)     printf("pin: [%s, %d]: %s: %d\n", __FUNCTION__, __LINE__, #a, (a))
+#define PINS(a)     printf("pin: [%s, %d]: %s: %s\n", __FUNCTION__, __LINE__, #a, (a))
 #else
-#define PINT()
-
+#define PINT(a)
+#define PINS(a)
 #endif
+
+int main() {
+
+    PINT(100);
+    PINS("Hellow world");
+
+    return 1;
+}
